1bc76de54b26626da5c34c10337b299422844e87
PeiniCheng <peini.cheng@mail.mcgill.ca>
2022-11-19 14:48:27 -0500

    [Server] list to queue

--- mbti-matchr-server/MbtiMatchr-Server/src/main/java/com/bugzappr/mbtimatchr/controller/SessionController.java
+++ mbti-matchr-server/MbtiMatchr-Server/src/main/java/com/bugzappr/mbtimatchr/controller/SessionController.java
@@ -6,9 +6,11 @@ import com.bugzappr.mbtimatchr.model.QueuePlayer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +19,10 @@ import org.springframework.web.context.request.async.DeferredResult;
 @RestController
 @RequestMapping("/api")
 public class SessionController {
-  private List<QueuePlayer> queue = new ArrayList<QueuePlayer>();
+  private ConcurrentLinkedDeque<QueuePlayer> queue = new ConcurrentLinkedDeque<QueuePlayer>();
   private ExecutorService matchers = Executors.newFixedThreadPool(6);
 
-  @GetMapping("/join")
+  @PostMapping("/join")
   public DeferredResult<QueueResponse> join(@RequestParam String mbti) {
     DeferredResult<QueueResponse> output = new DeferredResult<>(20000L);
     QueuePlayer player = new QueuePlayer(mbti, UUID.randomUUID());
